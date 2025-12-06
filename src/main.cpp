e8526bc0732dba50e3a676876f3e4a91070c9fcc
vividskies <itsvividskies@gmail.com>
2025-12-07 00:08:55 +1100

    setup SDL3

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(int, char**) {
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window* Window{SDL_CreateWindow(
+    "Hello Window", 800, 300, 0
+  )};
+
+  bool IsRunning = true;
+  SDL_Event Event;
+  while (IsRunning) {
+    while (SDL_PollEvent(&Event)) {
+      if (Event.type == SDL_EVENT_QUIT) {
+        IsRunning = false;
+      }
+    }
+  }
+
+  SDL_DestroyWindow(Window);
+  SDL_Quit();
+
+  return 0;
+}
